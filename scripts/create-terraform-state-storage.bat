e656cf4c939bb5c3e6c5d05e014680b311d465b6
Adam Rush <adamrushuk@gmail.com>
2019-08-01 21:54:19 +0100

    Added quotes for pipeline var replacement

--- scripts/create-terraform-state-storage.bat
+++ scripts/create-terraform-state-storage.bat
@@ -4,11 +4,11 @@
 :: For batch files use the prefix "call" before every azure command.
 
 :: Resource Group
-call az group create --location eastus --name $(terraform_storage_rg)
+call az group create --location eastus --name "$(terraform_storage_rg)"
 
 :: Storage Account
-call az storage account create --name $(terraform_storage_account) --resource-group $(terraform_storage_rg) ^
+call az storage account create --name "$(terraform_storage_account)" --resource-group "$(terraform_storage_rg)" ^
 --location eastus --sku Standard_LRS
 
 :: Storage Container
-call az storage container create --name terraform --account-name $(terraform_storage_account)
+call az storage container create --name terraform --account-name "$(terraform_storage_account)"
