b811e46bf26d4b3d8fc5a916851215d04b8d0a29
Adam Rush <adamrushuk@gmail.com>
2019-08-01 22:10:04 +0100

    Changed var replacement syntax with env vars

--- scripts/create-terraform-state-storage.bat
+++ scripts/create-terraform-state-storage.bat
@@ -3,12 +3,15 @@
 :: When using Windows agent in Azure DevOps, use batch scripting.
 :: For batch files use the prefix "call" before every azure command.
 
+:: Show env vars
+set
+
 :: Resource Group
-call az group create --location eastus --name "$(terraform_storage_rg)"
+call az group create --location eastus --name %TERRAFORM_STORAGE_RG%
 
 :: Storage Account
-call az storage account create --name "$(terraform_storage_account)" --resource-group "$(terraform_storage_rg)" ^
+call az storage account create --name %TERRAFORM_STORAGE_ACCOUNT% --resource-group %TERRAFORM_STORAGE_RG% ^
 --location eastus --sku Standard_LRS
 
 :: Storage Container
-call az storage container create --name terraform --account-name "$(terraform_storage_account)"
+call az storage container create --name terraform --account-name %TERRAFORM_STORAGE_ACCOUNT%
