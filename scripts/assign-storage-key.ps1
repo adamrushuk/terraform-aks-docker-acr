61c65ad607c1ea21243c9ef539b25b471e7a700f
Adam Rush <adamrushuk@gmail.com>
2019-08-01 22:24:52 +0100

    Changed pipeline replacement syntax with env vars

--- scripts/assign-storage-key.ps1
+++ scripts/assign-storage-key.ps1
@@ -1,7 +1,7 @@
 # Retrieve storage key for Terraform storage account
 
 # Get storage access key
-$key = (Get-AzureRmStorageAccountKey -ResourceGroupName "$(terraform_storage_rg)" -AccountName "$(terraform_storage_account)").Value[0]
+$key = (Get-AzureRmStorageAccountKey -ResourceGroupName "$env:TERRAFORM_STORAGE_RG" -AccountName "$env:TERRAFORM_STORAGE_ACCOUNT").Value[0]
 
 # Update Azure DevOps Pipeline variable
 Write-Host "##vso[task.setvariable variable=storage_key]$key"
