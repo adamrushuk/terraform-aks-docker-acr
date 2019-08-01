66c716641fbded509fcf89b68009d950b53ee52d
Adam Rush <adamrushuk@gmail.com>
2019-08-01 20:47:50 +0100

    Fixed var name

--- scripts/assign-storage-key.ps1
+++ scripts/assign-storage-key.ps1
@@ -4,4 +4,4 @@
 $key = (Get-AzureRmStorageAccountKey -ResourceGroupName $(terraform_storage_rg) -AccountName $(terraform_storage_account)).Value[0]
 
 # Update Azure DevOps Pipeline variable
-Write-Host "##vso[task.setvariable variable=storagekey]$key"
+Write-Host "##vso[task.setvariable variable=storage_key]$key"
