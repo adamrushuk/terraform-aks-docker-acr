a887c1f2846a3b87124879ce6a40196f06d58b66
Adam Rush <adamrushuk@gmail.com>
2019-08-15 07:31:19 +0100

    Added code to get/set latest AKS version available

--- scripts/create-terraform-state-storage.bat
+++ scripts/create-terraform-state-storage.bat
@@ -7,11 +7,15 @@
 set
 
 :: Resource Group
-call az group create --location eastus --name %TERRAFORM_STORAGE_RG%
+call az group create --location %RESOURCE_LOCATION% --name %TERRAFORM_STORAGE_RG%
 
 :: Storage Account
 call az storage account create --name %TERRAFORM_STORAGE_ACCOUNT% --resource-group %TERRAFORM_STORAGE_RG% ^
---location eastus --sku Standard_LRS
+--location %RESOURCE_LOCATION% --sku Standard_LRS
 
 :: Storage Container
 call az storage container create --name terraform --account-name %TERRAFORM_STORAGE_ACCOUNT%
+
+:: Get latest supported AKS version and update Azure DevOps Pipeline variable
+SET latest_aks_version=(call az aks get-versions -l %RESOURCE_LOCATION% --query 'orchestrators[-1].orchestratorVersion' -o tsv)
+@echo ##vso[task.setvariable variable=latest_aks_version]%latest_aks_version%
