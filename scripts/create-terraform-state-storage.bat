2b17929a52a33319854f1b7e4bd8b5e1b0bcb842
Adam Rush <adamrushuk@gmail.com>
2019-08-15 22:51:08 +0100

    Fixed assigning the output of a command into a batch file variable

--- scripts/create-terraform-state-storage.bat
+++ scripts/create-terraform-state-storage.bat
@@ -17,6 +17,8 @@ call az storage account create --name %TERRAFORM_STORAGE_ACCOUNT% --resource-gro
 call az storage container create --name terraform --account-name %TERRAFORM_STORAGE_ACCOUNT%
 
 :: Get latest supported AKS version and update Azure DevOps Pipeline variable
-SET latest_aks_version=(call az aks get-versions -l %RESOURCE_LOCATION% --query 'orchestrators[-1].orchestratorVersion' -o tsv)
-REM @echo ##vso[task.setvariable variable=latest_aks_version]%latest_aks_version%
-@echo ##vso[task.setvariable variable=latest_aks_version]1.14.5
+for /f %%i in ('call az aks get-versions -l %RESOURCE_LOCATION% --query "orchestrators[-1].orchestratorVersion" -o tsv') do @set latest_aks_version=%%i
+@echo Updating Pipeline variable with Latest AKS Version:
+@echo %latest_aks_version%
+
+@echo ##vso[task.setvariable variable=latest_aks_version]%latest_aks_version%
