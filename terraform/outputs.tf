85b062aa070f15e9b8b2538b4d27c8e724586768
Adam Rush <adamrushuk@gmail.com>
2019-08-20 12:11:45 +0100

    Added kubernetes_master_fqdn TF Output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,11 @@
-output "client_certificate" {
+output "kubernetes_master_fqdn" {
+  value = "${azurerm_kubernetes_cluster.aks.fqdn}"
+}
+
+output "kubernetes_client_certificate" {
   value = "${azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate}"
 }
 
-output "kube_config" {
+output "kubernetes_config" {
   value = "${azurerm_kubernetes_cluster.aks.kube_config_raw}"
 }
